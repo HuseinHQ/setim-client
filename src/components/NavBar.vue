69879a80ff59400680ba9f989e5fdd0d1fa62b7c
HuseinHQ <mhusein463@gmail.com>
2023-10-24 15:58:58 +0700

    Create navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="bg-dongker border-gray-200">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="#" class="flex items-center">
+        <img src="/steam.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SETIM</span>
+      </a>
+  </div>
+</nav></template>
